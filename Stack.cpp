8e6c5e06064b266ecdb0819fa2eda188725afdfd
RTCupid <19apr2006@vk.com>
2024-09-27 11:43:52 +0300

    + function StackPush (), and there is verification of complete all function with return int

--- Stack.cpp
+++ Stack.cpp
@@ -13,15 +13,31 @@ struct stack_t {stack_elem_t* data;
 
 int StackCtor (stack_t* stk, int startSize);
 
+int StackPush (stack_t* stk, stack_elem_t elem);
+
 int main ()
     {
     struct stack_t stk = {NULL,
                           0,
                           0};
 
-    StackCtor (&stk, 5);
+    if (!StackCtor (&stk, 5))
+        {
+        printf ("ERROR: Error of stack construction");
+        return -1;
+        }
 
-    //StackPush (&stk, 20);
+    if (!StackPush (&stk, 20))
+        {
+        printf ("ERROR: Error of push to stack");
+        return -1;
+        }
+
+    if (!StackPush (&stk, 30))
+        {
+        printf ("ERROR: Error of push to stack");
+        return -1;
+        }
 
     return 0;
     }
@@ -42,12 +58,37 @@ int StackCtor (stack_t* stk, int startCapacity)
     DBG printf ("Start: stk->size = %d\n", stk->size);
 
     stk->capacity = startCapacity;
-    DBG printf ("Start: stk->capacity = %d\n\n", stk->capacity);
+    DBG printf ("Start: stk->capacity = %d\n", stk->capacity);
 
     stk->data[0] = startElem;
     stk->size++;
 
-    DBG printf ("stk->data[0] = %lf\n", stk->data[0]);
+    DBG printf ("Start: stk->data[0] = %lf\n\n", stk->data[0]);
+
+    return 1;
+    }
+
+//Push elem to stack...........................................................
+
+int StackPush (stack_t* stk, stack_elem_t elem)
+    {
+    DBG printf ("Push %lf to stack\n", elem);
+
+    if (stk->size == stk->capacity)
+        {
+        stk->data = (stack_elem_t*)realloc (stk->data, stk->size);
+        if (stk->data == NULL)
+            {
+            printf ("ERROR: in StackPush realloc return 'NULL'\n");
+            return 0;
+            }
+        }
+
+    stk->data[stk->size] = elem;
+
+    DBG printf ("stk->data[%d] = %lf\n", stk->size, stk->data[stk->size]);
+
+    stk->size++;
 
     return 1;
     }
